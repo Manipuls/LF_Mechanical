e70c337ba1c257626f31d9821be44ec234ed1821
LiuFei <liufei_work@sina.com>
2017-03-10 11:54:08 +0800

    Test

---
+++ src/main/java/com/lf/controller/Test.java
@@ -0,0 +1,23 @@
+package com.lf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class Test {
+	
+	
+	@RequestMapping(value = "Test.html")
+	public void Test(){
+		
+		System.out.println("00000000000");
+		System.out.println("00000000000");
+		System.out.println("00000000000");
+		System.out.println("00000000000");
+		System.out.println("00000000000");
+	}
+
+}
---
+++ src/main/webapp/page/index.jsp
@@ -0,0 +1,21 @@
+<html>
+
+
+
+<head>
+<script type="text/javascript">
+
+function reqTest(){
+	
+	window.location.href= "Test.html";
+	
+}
+</script>
+</head>
+<body>
+<h2>Hello World!</h2>
+
+
+<input type="button" value="submit" onclick="reqTest()">
+</body>
+</html>
